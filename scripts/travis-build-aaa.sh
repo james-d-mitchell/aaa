903a309041729310aecf5e30c78b4927f17cd698
Luke <le27@st-andrews.ac.uk>
2020-02-05 16:31:49 +0000

    f

--- scripts/travis-build-aaa.sh
+++ scripts/travis-build-aaa.sh
@@ -6,13 +6,6 @@ set -e
 ################################################################################
 # Compile Digraphs and install Digraphs lib
 if [ "$SUITE" != "lint" ]; then
-  echo -e "\nCompiling the Digraphs package..."
-  cd $HOME/gap/pkg/digraphs
-  ./autogen.sh
-  ./configure $PKG_FLAGS $EXTRA_PKG_FLAGS
-  make
-  echo -e "\nDownloading $DIGRAPHS_LIB..."
-  curl --retry 5 -L -O https://gap-packages.github.io/Digraphs/$DIGRAPHS_LIB.tar.gz
-  tar xf $DIGRAPHS_LIB.tar.gz
-  rm $DIGRAPHS_LIB.tar.gz
+  echo -e "\nCompiling the Aaa package..."
+  cd $HOME/gap/pkg/aaa
 fi
